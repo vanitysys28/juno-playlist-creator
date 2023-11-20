83075a2adb4a2e7360dc291a34a0c5bdb54fc2b0
vanitysys28 <guiaubzer@gmail.com>
2023-11-20 23:22:55 +0100

    Adding environment variables and refactoring

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
+from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# URL = "https://www.junodownload.com/drumandbass/eight-weeks/releases/?items_per_page=100"
+# url = "https://www.junodownload.com/drumandbass/eight-weeks/releases/?items_per_page=100"
 
 # r = requests.get(url = URL)
 # soup = BeautifulSoup(r.text, features="html.parser")
@@ -11,7 +12,7 @@ from spotipy.oauth2 import SpotifyOAuth
 # duplicate = []
 # new = []
 
-# print("Script starting...")
+# print("Script starting")
 
 # while len(soup.find_all("a",{"title":"Next Page"})) > 0:
 #     r = requests.get(url = URL)
@@ -35,29 +36,26 @@ from spotipy.oauth2 import SpotifyOAuth
 # print(str(len(duplicate)) + " items already in song list")
 # print(str(len(new)) + " songs added to song list")
 
-scope = "playlist-modify-private,playlist-modify-public"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="763fa22eaf144d64bdbbdf3ba23e1f7c",client_secret="3f927ec9e15d4c00a99a75fba8805157",redirect_uri="http://localhost:8888/callback",scope=scope))
 
 
-file = open("drumoandbasso.txt",encoding="utf-8")
-lines = file.readlines()
-for line in lines:
-    try:
-        albumid = sp.search(q=line, limit=20,type='track')['tracks']['items'][0]['album']['id']
-        try:
-            albumtracks = sp.album_tracks(albumid)['items']
-            for tracks in albumtracks:
-                print(tracks['uri'])
-                playlistadd = sp.playlist_add_items("7z7rvFWfhOaD686m1VmHp8",[tracks['uri']],position=None)
-        except:
-            print("No match while searching for track")
-    except:
-        print('No match while searching for ' + line)
+
+# scope = "playlist-modify-private,playlist-modify-public"
+
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri="http://localhost:8888/callback",scope=scope))
+
+
+# file = open("drumoandbasso.txt",encoding="utf-8")
+# lines = file.readlines()
+# for line in lines:
+#     try:
+#         albumid = sp.search(q=line, limit=20,type='track')['tracks']['items'][0]['album']['id']
+#         try:
+#             albumtracks = sp.album_tracks(albumid)['items']
+#             for tracks in albumtracks:
+#                 print(tracks['uri'])
+#                 playlistadd = sp.playlist_add_items(PLAYLIST_ID,[tracks['uri']],position=None)
+#         except:
+#             print("No match while searching for track")
+#     except:
+#         print('No match while searching for ' + line)
     
-        
-    
-    # url = "https://api.spotify.com/v1/search?q=" + line + "&type=track"
-    # headers = {'Accept':'application/json','Content-Type':'application/json','Authorization': 'BQAXIr0QL3MWTihKdw75epse4AcRwF-B_nRtnPDbGtMBFZpTo6ltabE9xb9MCWvM5Y_vTlBN6VS76lkk25LSGdAYqgrFA5vh9SeL9JMKYwkCPJI7Q5cBabgmGRhH_6OUOzGYIw_t1dFBohEjGZzDxv_PD1bRR0sCzV4JJZXBidrBc0yRtszuPE8qC8qU4dWismCvIq26LMxq'}
-    # r = requests.get(url = url, headers = headers)
-    # print(r.text)
