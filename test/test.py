4b1678330f2dd672c4bc3a8c7cfe3106faef9028
vanitysys28 <guiaubzer@gmail.com>
2023-11-20 23:23:04 +0100

    Adding a test folder

---
+++ test/test.py
@@ -0,0 +1,4 @@
+ # url = "https://api.spotify.com/v1/search?q=" + line + "&type=track"
+    # headers = {'Accept':'application/json','Content-Type':'application/json','Authorization': 'BQAXIr0QL3MWTihKdw75epse4AcRwF-B_nRtnPDbGtMBFZpTo6ltabE9xb9MCWvM5Y_vTlBN6VS76lkk25LSGdAYqgrFA5vh9SeL9JMKYwkCPJI7Q5cBabgmGRhH_6OUOzGYIw_t1dFBohEjGZzDxv_PD1bRR0sCzV4JJZXBidrBc0yRtszuPE8qC8qU4dWismCvIq26LMxq'}
+    # r = requests.get(url = url, headers = headers)
+    # print(r.text)
