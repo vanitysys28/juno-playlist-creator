91eb53ed358cd709d7a23763bab7305f34daa8b7
vanitysys28 <guiaubzer@gmail.com>
2023-11-30 23:21:41 +0100

    creating unique data files everytime script is launched

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 import os
+import datetime
 from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
+now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 def scrapeJuno():
     url = "https://www.junodownload.com/drumandbass/eight-weeks/releases/?items_per_page=100"
@@ -11,12 +14,12 @@ def scrapeJuno():
 
     while len(soup.find_all("a",{"title":"Next Page"})) > 0:
         soup = BeautifulSoup(requests.get(url).text, features="html.parser")
-        file = open("data.txt","a",encoding="utf-8")
+        file = open("data/" + now + ".txt","a",encoding="utf-8")
                 
         for data in soup.find_all("div", {"class":"col-12 col-md order-4 order-md-3 mt-3 mt-md-0 pl-0 pl-md-2"}):
             entry = data("div", {"class":"col juno-artist"})[0].get_text().replace("/", " ") + "    " + data("a", {"class":"juno-title"})[0].get_text()
 
-            with open('data.txt') as f:
+            with open('data/' + now + '.txt') as f:
                 if entry not in f.read():
                     file.write(entry)
                     file.write('\n')
@@ -35,7 +38,7 @@ def addToSpotifyPlaylist():
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id,client_secret,redirect_uri="http://localhost:8888/callback",scope=scope))
 
-    file = open("data.txt",encoding="utf-8")
+    file = open("data/" + now + ".txt",encoding="utf-8")
     lines = file.readlines()
     for line in lines:
             albumid = sp.search(q=line, limit=20,type='track')['tracks']['items'][0]['album']['id']
